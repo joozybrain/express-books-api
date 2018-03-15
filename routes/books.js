8699dfd19c3533d75194927a261a63ddf3135e39
Benjamin Ang <benjamin.ang@gmail.com>
2018-03-16 00:09:25 +0800

    refactor code to use async await

--- routes/books.js
+++ routes/books.js
@@ -4,68 +4,62 @@ const Book = require("../models/books_models");
 var router = express.Router();
 
 /* GET books listing. */
-router.get("/", function(req, res) {
-  Book.find({}, function(err, books) {
-    if (err) throw err;
-    res.json({ message: "respond with all books", books: books });
-  });
-});
-
-router.get("/", function(req, res) {
-  Book.find({})
-    .then(function(books) {
-      res.json({ message: "respond with all books", books: books });
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
 router.get("/", async function(req, res) {
-  const books = await Book.find({});
-
   try {
+    const books = await Book.find({});
     res.json({ message: "respond with all books", books: books });
   } catch (err) {
     res.json(err);
   }
 });
 
-router.get("/:id", function(req, res) {
-  Book.findById(req.params.id, function(err, book) {
-    if (err) throw err;
-    res.json({ message: `get book with id ${book}` });
-  });
+router.get("/:id", async function(req, res) {
+  try {
+    const bookFound = await Book.findById(req.params.id);
+    res.json({ message: bookFound });
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-router.post("/", function(req, res) {
+router.post("/", async function(req, res) {
   let newBook = new Book({
     title: req.body.Title
   });
-
-  newBook.save(function(err) {
-    if (err) throw err;
-    console.log("book saved");
-  });
-
-  res.json({ message: `create new book using data from ${req.body.Title}` });
+  try {
+    await newBook.save();
+    res.json({
+      message: `create new book using data from ${req.body.Title}`
+    });
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-router.put("/:id", function(req, res) {
-  Book.findByIdAndUpdate(req.params.id, { title: req.body.Title }, function(
-    err,
-    book
-  ) {
-    if (err) throw err;
-    res.json({ message: `update book with id ${req.params.id}` });
-  });
+router.put("/:id", async function(req, res) {
+  try {
+    const bookUpdate = await Book.findByIdAndUpdate(
+      req.params.id,
+      { title: req.body.Title },
+      { new: true }
+    );
+    res.json({ message: bookUpdate });
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-router.delete("/:id", function(req, res) {
-  Book.findByIdAndRemove(req.params.id, function(err, book) {
-    if (err) throw err;
-    res.json({ message: `delete book with id ${req.params.id}` });
-  });
+router.delete("/:id", async function(req, res) {
+  try {
+    const bookDeleted = await Book.findByIdAndRemove(req.params.id);
+    if (bookDeleted !== null) {
+      res.json({ message: `Book deleted`, books: bookDeleted });
+    } else {
+      res.json({ message: "Book not found" });
+    }
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
